aa10853094e875dcdbd048bdfc84ecb3c182eff3
loroka <mulldo53@gmail.com>
2019-08-05 23:16:59 +0200

    Add acceleration, deceleration and brake V2
    speed is now calculated using engine force and environmental resistances, like air drag and rolling resistance

--- car.py
+++ car.py
@@ -1,52 +1,91 @@
 import pygame
+import pygame.sprite as sprite
 
-class Car(pygame.sprite.Sprite):
-    def __init__(self, max_health, max_speed, min_speed):
+from pygame import Vector2
+
+class Car(sprite.Sprite):
+    def __init__(self):
         super().__init__()
-        # health
-        self._max_health = max_health
-        self._health = max_health
-
-        # velocity
-        self._max_speed = max_speed
-        self._min_speed = min_speed
-        self.speed = 0
-        self._acceleration = 0.5
-        self._deacceleration = 3.0
-
-        # steering
-        self.rotation = 0
-        self.steering_strenght = 3
-        self.last_rot_update = 0
-
+        
+        self.mass = 1000  # vehicle weight
+        self.engine_f = 0
+        self.max_engine_f = 100
+        self.brake_f = 50
+        self.acceleration_speed = 5
+        self.air_resistance = 1
+        self.roll_resistance = 30 # aprox 30 times air resistance
+        self.direction = Vector2((0, -1))
+        self.velocity = Vector2((0.0,0.0))
+        self.f_drag = Vector2((0,0))
+        self.f_res = Vector2((0,0))
+        self.speed = 0.0
+        self.traction = Vector2((0,0))
+        self.forward = True
+        
         # asset
-        self.original_image = pygame.image.load("assets/Car.png")
+        self.original_image = pygame.image.load("assets/Car.png").convert()
         self.original_image = pygame.transform.scale(self.original_image, (48, 48))
-        self.original_image.set_colorkey((0,0,0))
         self.image = self.original_image
-        self.image.set_colorkey((0,0,0))
+        self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect()
-        
-    def rotate(self, angle):
-        """
-        Rotate car by angle
+        self.rect = (200, 500) 
 
-        :param angle: int
+    def accelerate(self):
         """
-
-        self.rotation = (self.rotation + angle) % 360
-        new_image = pygame.transform.rotate(self.original_image, self.rotation)
-        old_center = self.rect.center
-        self.image = new_image
-        self.rect = self.image.get_rect()
-        self.rect.center = old_center
+        Increase car engine force, if reversing brakes
+        """
+        if self.speed > 0.1 and not self.forward: # brake
+            self.engine_f += self.brake_f
+            if self.engine_f > 0:
+                self.engine_f = 0
+        else: # go front
+            self.engine_f += self.acceleration_speed
+            if self.engine_f > self.max_engine_f:
+                self.engine_f = self.max_engine_f
+            self.forward = True
     
-    def move_by(self, position):
+    def decelarate(self):
         """
-        Move car by position
-        
-        :param position: pygame.Vector2
+        Decelarate engine force
         """
-        self.rect.x += position.x
-        self.rect.y += position.y
+        if self.speed > 0.1 and self.forward:
+            self.engine_f -= self.acceleration_speed * 2
+            if self.engine_f < 0:
+                self.engine_f = 0
+        elif self.speed > 0.1 and not self.forward:
+            self.engine_f += self.acceleration_speed * 2
+            if self.engine_f > 0:
+                self.engine_f = 0
+            
+    def brake(self):
+        """
+        Brakes car or accelerate backwards
+        """
+        if self.speed > 0.1 and self.forward: # brake
+            self.engine_f -= self.brake_f
+            if self.engine_f < 0:
+                self.engine_f = 0
+        else: # go reverse
+            self.engine_f -= self.acceleration_speed / 2
+            self.forward = False
 
+    def update(self):
+        """
+        Updates vehicle forces
+        """
+        self.traction = self.engine_f * self.direction
+        self.speed = self.velocity.dot(self.velocity)
+
+        # resistance
+        self.f_drag = -self.air_resistance * self.velocity * self.speed
+        self.f_res = -self.roll_resistance * self.velocity
+
+        # real force
+        longtitudinal_f = self.traction + self.f_drag + self.f_res
+        acceleration = longtitudinal_f / self.mass
+        
+        self.velocity += acceleration 
+
+        # change position
+        self.rect += self.velocity
+        
--- player.py
+++ player.py
@@ -1,31 +1,34 @@
 import pygame
+import pygame.sprite as sprite
+import pygame.font as font
+
+from pygame import Vector2
+
 from weapon import Weapon
 from car import Car
 
-class Player(pygame.sprite.Group):
+class Player(sprite.Group):
     def __init__(self, position):
         super().__init__()
         
         self._position = position
 
-        self.direction = pygame.Vector2((0, 1))
+        self.direction = Vector2((0, 1))
 
         x, y, z = position
 
-        self._car = Car(max_health=100, max_speed=10, min_speed=-10)
+        self._car = Car()
         self.add(self._car)
 
-        self._weapon = Weapon(self._car.rect.center)
-        self.add(self._weapon)
+        #self._weapon = Weapon(self._car.rect.center)
+        #self.add(self._weapon)
 
-        self.move_by(pygame.Vector2(x,y))
+        #self.move_by(pygame.Vector2(x,y))
 
         self.left = False
         self.right = False
         self.up = False
         self.down = False
-
-        self.last_speed_update = 0
 
 
     def update(self, key_state):
@@ -34,73 +37,44 @@ class Player(pygame.sprite.Group):
         """
         super().update()
         self._position_update()
-        self._rotation_update()
+        #self._rotation_update()
 
         self.up = key_state.get('up')
         self.down = key_state.get('down')
         self.left = key_state.get('left')
         self.right = key_state.get('right')
 
+        if self.up:
+            self._car.accelerate()
+        elif self.down:
+            self._car.brake()
+        else:
+            self._car.decelarate()
+
    
     def _position_update(self):
         """
         Updates player position proportional to speed
         """ 
-        now = pygame.time.get_ticks()
-        if now - self.last_speed_update > self._car._max_speed - self._car.speed:
-        
-            if self.down: # backward
-                self._car.speed += self._car._acceleration
-                if self._car.speed > self._car._max_speed:
-                    self._car.speed = self._car._max_speed
-            elif self.up: # forward
-                self._car.speed -= self._car._acceleration 
-                if self._car.speed < self._car._min_speed:
-                    self._car.speed = self._car._min_speed
-            else: # breaking with no button pushed
-                if self._car.speed < 0:
-                    self._car.speed += self._car._deacceleration
-                    if self._car.speed > 0:
-                        self._car.speed = 0
-                elif self._car.speed > 0:
-                    self._car.speed -= self._car._deacceleration
-                    if self._car.speed < 0:
-                        self._car.speed = 0
-                
-            self.last_speed_update = now
-  
-        if self._car.speed != 0:
-            self.move_by(self.direction * self._car.speed)
-           
+        self._car.update()
             
-    def _rotation_update(self):
+    def draw(self, window):
         """
-        Checks if key is pressed, if so rotates all sprites
+        Draws all sprites in group and renders player info
         """
-        if self.left:
-            angle = self._car.steering_strenght
-        elif self.right:
-            angle = -self._car.steering_strenght
-        else:
-            return
+        super().draw(window)
 
-        self.direction = self.direction.rotate(-angle)
-
-        for sprite in self.sprites():
-            sprite.rotate(angle)
-
-
-    def move_by(self, position):
-        """
-        Moves all sprites by given position
-        """
-        for sprite in self.sprites():
-            sprite.move_by(position)
+        # print player info
+        myfont = font.SysFont('Comic Sans MS', 15)
+        infos = []
+        infos.append(myfont.render(f"traction: {self._car.traction}", False, (255, 255, 255)))
+        infos.append(myfont.render(f"speed: {self._car.speed:.4f}", False, (255, 255, 255)))
+        infos.append(myfont.render(f"velocity: {self._car.velocity}", False, (255, 255, 255)))
+        infos.append(myfont.render(f"forward: {self._car.forward}", False, (255, 255, 255)))
         
-        # fix gun in middle of car, weapon is shifted a bit
-        # during rotation
-        self._weapon.rect.center = self._car.rect.center 
 
+        for i, info in enumerate(infos):
+            window.blit(info, (0,20 * i))
 
         
 
